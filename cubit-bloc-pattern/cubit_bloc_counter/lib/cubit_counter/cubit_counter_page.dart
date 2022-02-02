480e15d688c4f7c12063a1d7d97273f9732bbcb3
Andy Eckhardt <eckhardtandy@gmail.com>
2022-02-02 20:31:56 +0100

    cubit_bloc_counter: Folder structure changed

---
+++ cubit-bloc-pattern/cubit_bloc_counter/lib/cubit_counter/cubit_counter_page.dart
@@ -0,0 +1,84 @@
+import 'package:cubit_bloc_counter/cubit_counter/cubit/counter_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider<CounterCubit>(
+      create: (context) => CounterCubit(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Flutter Cubit Counter',
+          ),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Counter value:',
+              ),
+              BlocConsumer<CounterCubit, CounterState>(
+                listener: (context, state) {
+                  if (state.wasIncremented == null) {
+                    return;
+                  }
+                  if (state.wasIncremented!) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text('incremented'),
+                        duration: Duration(
+                          microseconds: 500,
+                        ),
+                      ),
+                    );
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text('decremented'),
+                        duration: Duration(
+                          microseconds: 500,
+                        ),
+                      ),
+                    );
+                  }
+                },
+                builder: (context, state) {
+                  return Text(
+                    state.counterValue.toString(),
+                    style: Theme.of(context).textTheme.headline4,
+                  );
+                },
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  FloatingActionButton(
+                    onPressed: () {
+                      context.read<CounterCubit>().decrement();
+                    },
+                    tooltip: 'Decrement',
+                    child: const Icon(Icons.remove),
+                  ),
+                  FloatingActionButton(
+                    onPressed: () {
+                      context.read<CounterCubit>().increment();
+                    },
+                    tooltip: 'Increment',
+                    child: const Icon(Icons.add),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
