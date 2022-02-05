703c8ca66c42483087d06faee9e71c1e0e76666e
Andy Eckhardt <eckhardtandy@gmail.com>
2022-02-05 20:39:29 +0100

    cubit_bloc_counter: bloc counter page added

--- cubit-bloc-pattern/cubit_bloc_counter/lib/bloc_counter/bloc_counter_page.dart
+++ cubit-bloc-pattern/cubit_bloc_counter/lib/bloc_counter/bloc_counter_page.dart
@@ -1,1 +1,60 @@
+import 'package:cubit_bloc_counter/bloc_counter/bloc/counter_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+class BlocCounterPage extends StatelessWidget {
+  const BlocCounterPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider<CounterBloc>(
+      create: (context) => CounterBloc(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Flutter Bloc Counter',
+          ),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Counter value:',
+              ),
+              BlocBuilder<CounterBloc, CounterState>(
+                builder: (context, state) {
+                  return Text(
+                    state.counterValue.toString(),
+                    style: Theme.of(context).textTheme.headline4,
+                  );
+                },
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  FloatingActionButton(
+                    onPressed: () {
+                      context.read<CounterBloc>().add(DecrementEvent());
+                    },
+                    tooltip: 'Decrement',
+                    child: const Icon(Icons.remove),
+                  ),
+                  FloatingActionButton(
+                    onPressed: () {
+                      context.read<CounterBloc>().add(IncrementEvent());
+                    },
+                    tooltip: 'Increment',
+                    child: const Icon(Icons.add),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
